8995d54ebba76d2407e13eb485256f5f47d965e7
Facundo Monti <facundomonti03@gmail.com>
2023-10-30 10:12:04 -0300

    arreglado dl despliegue de los botones de la pagina de promociones

--- Stelarium/promocions/mostrar-pro.js
+++ Stelarium/promocions/mostrar-pro.js
@@ -1,10 +1,17 @@
-function mostrar(){
-    const faqQuestions = document.querySelectorAll('.promocion');
-        faqQuestions.forEach(question => {
-            question.addEventListener('click', () => {
-                question.classList.toggle('active');
-                const answer = question.nextElementSibling;
-                answer.classList.toggle('show');
-            });
-        });
-}
+// Obtener todas las preguntas
+var preguntas = document.querySelectorAll('.promos .promocion-item');
+
+// Agregar un evento de clic a cada pregunta
+preguntas.forEach(function(pregunta) {
+    pregunta.addEventListener('click', function() {
+        // Encontrar la respuesta correspondiente
+        var respuesta = pregunta.querySelector('.promocion');
+
+        // Toggle para mostrar u ocultar la respuesta con efecto de desplazamiento
+        if (respuesta.style.maxHeight === '0px') {
+            respuesta.style.maxHeight = respuesta.scrollHeight + 'px';
+        } else {
+            respuesta.style.maxHeight = '0';
+        }
+    });
+});
